987d5cb3b640eea2f91221db62b82cc86e67d76c
fabiananunesrita <131107627+fabiananunesrita@users.noreply.github.com>
2024-04-01 12:01:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("mostrar-acompanhamentos").addEventListener("click", function() {
+    document.getElementById("lista-acompanhamentos").classList.toggle("escondido");
+    document.getElementById("pagar").classList.toggle("escondido");
+});
